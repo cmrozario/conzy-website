a1c4f6b5a4e159a16432e6128659b1cda3543a44
cmrozario <crozario@mit.edu>
2024-03-05 01:44:56 -0500

    h

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,36 @@
+
+<svelte:head>
+	<title>My projects</title>
+</svelte:head>
+
+<h1>
+    Projects
+</h1>
+<div class="projects">
+    <article>
+        <h2>A2: Exploratory Data Analysis</h2>
+        <a href = "A2_report.html">Link to page</a>
+    </article>
+
+    <article>
+        <h2>A3: Visualization Design</h2>
+        <a href = "A3_report.html">Link to page</a>
+        <p> </p>
+    </article>
+    <!-- <article>
+        <h2>Beatae labore nihil iusto.</h2>
+        <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
+        <p>Corporis repudiandae vero quas animi id, quos accusamus soluta sed eaque tempore quis eligendi suscipit maiores incidunt eum sequi delectus. Quibusdam libero esse, illum quod magnam id non repellat error.</p>
+    </article>
+    <article>
+        <h2>Aspernatur assumenda ullam quas.</h2>
+        <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
+        <p>In dicta porro quidem dolores, perferendis earum vitae quasi tempore eveniet! Eos aut alias explicabo perspiciatis cumque dicta? Alias at maxime possimus ipsa error beatae deleniti magni earum adipisci dolores!</p>
+    </article>
+    <article>
+        <h2>Mollitia ullam vel voluptates!</h2>
+        <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
+        <p>Laboriosam doloribus optio delectus illo soluta aliquam impedit eius mollitia ratione, quibusdam, nam eligendi numquam, voluptates vero perspiciatis perferendis tenetur vel accusamus iure quae. Minus laborum velit rem harum tempore.</p>
+    </article> -->
+
+</div>
