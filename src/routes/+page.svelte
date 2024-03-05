44bcddb982a7b9255114284c3f8724fb61fca48a
cmrozario <crozario@mit.edu>
2024-03-05 01:29:44 -0500

    hello

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<h1>
+    Conzy
+</h1>
+<p>
+    I am a 21 year old senior studying Math and AI at MIT. I love playing volleyball, with my dog (DJ), sometimes with Colin Weaver, and sometimes with my brother (Chris). 
+</p>
+<img src = "images/IMG_2430.jpg" alt = "a VERY cute picture of my dog DJ" width = "200">
+<img src = "images/IMG_3672.jpg" alt = "a picture of me and my brother at his first volleyball tournament" width = "200" >
+<img src = "images/IMG_2608.jpg" alt = "a picture of Colin Weaver, my boyfriend of 3 years" width = "200" >
+<img src = "images/MIT Womens Volleyball 2022 NCAA Sweet 16 (Tufts)-1110_Original.jpg" alt = "a picture of me playing volleyball at Tufts university" width = "300">
+
