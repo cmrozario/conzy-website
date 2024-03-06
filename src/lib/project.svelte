cb9fcd3a8e56192061d3fb6843bd7b9e6c1739a4
cmrozario <crozario@mit.edu>
2024-03-05 19:43:08 -0500

    final

--- src/lib/project.svelte
+++ src/lib/project.svelte
@@ -1,26 +1,23 @@
-
-
-<article>
-    <h2>{p.title}</h2>
-    <img src={p.image} alt="">
-    <p><a href = "A2_report.html">{p.description}</a></p>
-</article>
-
 <script>
     export let info = {};
     let p = info;
     export let hLevel = 2;
 </script>
 
+<article>
+    <svelte:element this={"h" + hLevel}>{info.title}</svelte:element>
+    <img src={p.image} alt="">
+    <p><a href = "A2_report.html">{p.description}</a></p>
+</article>
 
-<svelte:element this={"h" + hLevel}>{info.title}</svelte:element>
+
+
+
+
 
 
 <style>
-.projects{
-	display: grid;
-	grid-template-columns: repeat(auto-fill, minmax(15em, 1fr));
-}
+
 
 article{
 	display:grid;
