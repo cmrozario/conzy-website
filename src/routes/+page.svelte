fee0d9d39c05562272422d30c4756475fbd9dba5
cmrozario <crozario@mit.edu>
2024-03-05 19:36:31 -0500

    home projects

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,6 +13,17 @@
 <p>
     Hello, I am a 21 year old senior studying Math and AI at MIT. I love playing volleyball, with my dog (DJ), sometimes with Colin Weaver, and sometimes with my brother (Chris). 
 </p>
+
+<h1>
+    A couple recent projects:
+</h1>
+<div class="projects">
+    {#each projects.slice(0,3) as p}
+    <Project info={p} />
+    {/each}
+   
+</div>
+
 <img src = "images/IMG_2430.jpg" alt = "a VERY cute picture of my dog DJ" width = "200">
 <img src = "images/IMG_3672.jpg" alt = "a picture of me and my brother at his first volleyball tournament" width = "200" >
 <img src = "images/IMG_2608.jpg" alt = "a picture of Colin Weaver, my boyfriend of 3 years" width = "200" >
